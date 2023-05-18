1b24655a62fe07cf47ac3f6883a9a9fe28d6f351
Jiahua Jiang <jiahua212@gmail.com>
2023-05-17 17:16:10 -0700

    f

--- pixi-game/src/Entities/textures.js
+++ pixi-game/src/Entities/textures.js
@@ -3,3 +3,4 @@ import * as PIXI from 'pixi.js';
 
 export const alienTexture = PIXI.Texture.from('https://raw.githubusercontent.com/2guud4u/abducted/newBranch/pixi-game/src/Entities/skins/happyAlien.png');
 export const characterTexture = PIXI.Texture.from('https://raw.githubusercontent.com/2guud4u/abducted/newBranch/pixi-game/src/Entities/skins/main.png');
+export const ropeTexture = PIXI.Texture.from('https://raw.githubusercontent.com/2guud4u/abducted/newBranch/pixi-game/src/Entities/skins/rope.png');
